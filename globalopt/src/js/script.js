2f265e30c1f2a73e04665a0bc1512c09f64b5969
Ash Shvoyan <67926571+Scorb@users.noreply.github.com>
2021-03-29 19:15:26 +0400

    Add files via upload

---
+++ globalopt/src/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    centerMode: true,
+    centerPadding: '80px',
+    slidesToShow: 1,
+    speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
